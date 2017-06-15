6ff5aa7f6837d1d01ec06069b9875ce15fa4df1b
ekotwick <edmond.kotwick@gmail.com>
2017-06-15 17:31:44 -0400

    add binary tree and binary tree tests

--- CrackCodeInterview/Trees&Graphs.js
+++ CrackCodeInterview/Trees&Graphs.js
@@ -1,15 +1,65 @@
 'use strict';
 
-class BTNode {
-	constructor(val) {
-		this.value = val
-	}
-}
-
 class BinaryTree {
-	constructor() {
+	constructor(val) {
+		this.value = val;
+		this.length = 1;
+		this.right = null;
+		this.left = null;
+	}
+
+	insert(val) {
+		this.length += 1;
+		let num = Math.random();
+		let direction = num > .5 ? 'right' : 'left';
+		if (direction === 'right') {
+			if (this.right) this.right.insert(val); 
+			else this.right = new BinaryTree(val);
+		} else if (direction === 'left') {
+			if (this.left) this.left.insert(val);
+			else this.left = new BinaryTree(val);
+		}
+	}
+
+	print() {
+		return JSON.stringify(this, null, 2);
+	}
+
+	find(val, traversal='depth-first', order='pre-order') {
+		this.values = [];
+		const pusher = (curVal) => this.values.push(val === curVal);
+
+		if (traversal === 'depth-first') {
+			if (order === 'pre-order') this.dft(pusher, 'pre-order');
+			if (order === 'in-order') this.dft(pusher, 'in-order');
+			if (order === 'post-order') this.dft(pusher, 'post-order');
+		} else {
+			this.bft(pusher);
+		}
+
+		const combiner = (el1, el2) => el1 || el2;
+
+		return this.values.reduce((el1, el2) => combiner(el1, el2), false);
+	}
+
+	bft(callback) {
+		let queue = [this]; // start with the thing loaded
+		while (queue.length) {
+			let current = queue.shift();
+			if (current.left) queue.push(current.left);
+			if (current.right) queue.push(current.right);
+			callback(current.value);
+		}
+	}
+
+	dft(callback, order) {
+		if (order === 'pre-order') callback(this.value);
+		if (this.left) this.left.dft(callback, order);
+		if (order === 'in-order') callback(this.value);
+		if (this.right) this.right.dft(callback, order);
+		if (order === 'post-order') callback(this.value);
 
 	}
 }
 
-module.exports = { BTNode, BinaryTree }
+module.exports = { BinaryTree }
--- CrackCodeInterview/Trees&Graphs.spec.js
+++ CrackCodeInterview/Trees&Graphs.spec.js
@@ -1,43 +1,55 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 const path = require('./Trees&Graphs');
 
-const BTNode = path.BTNode;
 const BinaryTree = path.BinaryTree;
 
 describe('BinaryTree', () => {
 	let bt;
 
 	beforeEach(() => {
-		bt = new BinaryTree();
+		bt = new BinaryTree(10);
 	});
 
-	it('should have `root` and `length` properties', () => {
-		expect(bt).to.have.own.property('root');
+	it('should have `length`, `value`, `left`, and `right` properties', () => {
 		expect(bt).to.have.own.property('length');
+		expect(bt).to.have.own.property('value');
+		expect(bt).to.have.own.property('left');
+		expect(bt).to.have.own.property('right');
 	});
 
-	it('should have `insert`, `remove`, and `find` methods', () => {
+	it('should have `insert`, `find`, breadth first traversal, and depth first traversal methods', () => {
 		expect(bt.insert).to.be.a('function');
-		expect(bt.insert).to.be.a('remove');
-		expect(bt.insert).to.be.a('find');
+		expect(bt.insert).to.be.a('function');
+		expect(bt.bft).to.be.a('function');
+		expect(bt.dft).to.be.a('function');
 	});
 
 	it('—`insert` should add elements to the tree', () => {
+		expect(bt.length).to.eql(1);
 		bt.insert(1);
 		bt.insert(2);
 		bt.insert(3);
-		expect(bt.length).to.eql(3);
+		expect(bt.length).to.eql(4);
 		bt.insert(4);
 		bt.insert(5);
-		expect(bt.length).to.eql(5);
+		expect(bt.length).to.eql(6);
 	});
 
-	it('—`remove` should remove elements from the tree', () => {
-
-	});
-
-	it('—`find` should determine whether an element exists in the tree', () => {
-
+	it('—`find` should use breadth first search or depth first search to determine whether an element exists in the tree', () => {
+		bt.insert(1);
+		bt.insert(2);
+		bt.insert(3);
+		bt.insert(4);
+		bt.insert(5);
+		expect(bt.find(5, 'depth-first', 'pre-order')).to.eql(true);
+		expect(bt.find(4, 'depth-first', 'in-order')).to.eql(true);
+		expect(bt.find(3, 'depth-first', 'post-order')).to.eql(true);
+		expect(bt.find(2, 'breadth-first')).to.eql(true);
+		expect(bt.find(1)).to.eql(true);
+		expect(bt.find(6)).to.eql(false);
+		expect(bt.find(0)).to.eql(false);
 	});
 });
